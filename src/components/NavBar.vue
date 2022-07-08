d8d245ebd971672e89b15a77c5d634783e4a41a4
Fabio Matsumoto <hide.fabio123@gmail.com>
2022-07-07 23:01:41 -0300

    feat: initial environment

---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<script>
+import IconGithub from "./icons/IconGithub.vue";
+
+export default {
+	components: {
+		IconGithub,
+	},
+};
+</script>
+
+<template>
+	<nav class="navbar navbar-dark bg-dark navbar-expand-xl">
+		<div class="logo-container">
+			<IconGithub class="github-icon" />
+			<h3 class="title"><strong>Github</strong> Search Profile</h3>
+		</div>
+	</nav>
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+.navbar {
+	max-height: 70px;
+}
+.logo-container {
+    display: flex;
+    align-items: center;
+}
+.github-icon {
+	width: 40px;
+	height: 40px;
+    margin: 10px;
+    cursor: pointer;
+	color: var(--vt-c-white-mute);
+}
+.title {
+	color: var(--vt-c-white-mute);
+    cursor: pointer;
+	font-weight: 300;
+}
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<script>
+import SVGNotFound from "../components/icons/NotFound.vue";
+
+export default {
+	components: {
+		SVGNotFound,
+	},
+};
+</script>
+
+<template>
+	<main>
+		<SVGNotFound />
+		<h2>NOT FOUNDD</h2>
+	</main>
+</template>
+
+<style></style>
