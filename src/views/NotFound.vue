5632beb6900ad6d166febcd74d29d061d66f64ef
Fabio Matsumoto <hide.fabio123@gmail.com>
2022-07-09 15:18:46 -0300

    feat: add not found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,12 +1,31 @@
 <script lang="ts">
+import NotFoundSVG from '../assets/NotFound.svg'
 export default {
 	name: "NotFound",
+	components: {
+		NotFoundSVG
+	}
 };
 </script>
 <template>
 	<main>
-		<h2>NOT FOUNDD</h2>
+		<NotFoundSVG/>
+		<a href="/">Back to home</a>
 	</main>
 </template>
 
-<style></style>
+<style>
+main {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+svg {
+	width: 500px;
+}
+a {
+	font-size: 16px;
+	text-decoration: none;
+	color: #c9d1d9;
+}
+</style>
